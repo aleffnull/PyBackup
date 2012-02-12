cb3e2b69765aa36edefd8acdbfa2e8d8ef86620f
Mikhail Savkin <savkinmk@gmail.com>
2012-02-12 21:16:31 +0400

    Repository name is determined.

--- pybackup.py
+++ pybackup.py
@@ -16,19 +16,26 @@ class BackuperException(Exception):
 
 class Backuper:
 	def __init__(self, repoPath):
-		self.__repoPath = path.abspath(repoPath)
+		self.__repoPath = path.normpath(path.abspath(repoPath))
 
 	def run(self):
 		self.__runChecks()
-		print "Creating backup of repository %s" % self.__repoPath
+		print "Creating backup of repository '%s'" % self.__repoPath
 
-		repoName = path.dirname(self.__repoPath)
-		print "Repository name is %s" % repoName
+		repoName = self.__getDirectoryName(self.__repoPath)
+		print "Repository name is '%s'" % repoName
 
 	def __runChecks(self):
 		if not path.isdir(self.__repoPath):
 			message = "path to repository should be an existing directory: %s" % self.__repoPath
 			raise BackuperException(message)
+
+	def __getDirectoryName(self, directoryPath):
+		dummyFile = path.join(directoryPath, "dummy.txt")
+		directory = path.dirname(dummyFile)
+		directoryName = path.split(directory)[1]
+
+		return directoryName
 
 def main():
 	parser = OptionParser()
