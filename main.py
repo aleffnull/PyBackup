5456f6b6be8858ac4173111a40f8a52d96db3daa
Mikhail Savkin <savkinmk@gmail.com>
2012-04-05 18:03:29 +0400

    Configure logging.

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 """ Backup local git repositories to CD on Windows """
 
 __author__ = "Mikhail K. Savkin"
+__version__ = "1.0"
 
-from Backuper import Backuper
-from BackuperException import BackuperException
-from optparse import OptionParser
+import logging
+import logging.config
 
 def main():
-	parser = OptionParser()
-	parser.add_option("-r", "--repo", dest="repoPath", help="path to repository")
-	(options, args) = parser.parse_args()
+	logging.config.fileConfig("logging.conf")
+	log = logging.getLogger()
+	log.info("PyBackup %s by %s" % (__version__, __author__))
+	log.info("Execution finished")
 
-	if options.repoPath is None:
-		parser.error("path to repository is required")
-
-	backuper = Backuper(options.repoPath)
-	try:
-		backuper.run()
-	except BackuperException as exception:
-		print "Error occurred: %s" % exception.message
+#	parser = OptionParser()
+#	parser.add_option("-r", "--repo", dest="repoPath", help="path to repository")
+#	(options, args) = parser.parse_args()
+#
+#	if options.repoPath is None:
+#		parser.error("path to repository is required")
+#
+#	backuper = Backuper(options.repoPath)
+#	try:
+#		backuper.run()
+#	except BackuperException as exception:
+#		print "Error occurred: %s" % exception.message
 
 if __name__ == "__main__":
 	main()
