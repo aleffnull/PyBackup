b9543e8a46a3894a74d18953e021d1d4c022d63d
Mikhail Savkin <savkinmk@gmail.com>
2012-04-06 17:34:49 +0400

    Change test names.

--- Tests/DirectoryUtilsTests.py
+++ Tests/DirectoryUtilsTests.py
@@ -5,37 +5,37 @@ from DirectoryUtils import GetFullAbsolutePath, GetDirectoryName
 
 class GetFullPathTests(unittest.TestCase):
 
-	def testIsNotEmpty(self):
+	def test_ResultIsNotEmpty(self):
 		result = GetFullAbsolutePath(".")
 		self.assertNotEqual(len(result), 0)
 
-	def testIsAbsoluteForCurrentDirectory(self):
+	def test_CurrentDirectory_IsAbsolute(self):
 		result = GetFullAbsolutePath(".")
 		self.assertFalse("." in result)
 
-	def testIsAbsoluteForParentDirectory(self):
+	def test_ParentDirectory_IsAbsolute(self):
 		result = GetFullAbsolutePath("..")
 		self.assertFalse(".." in result)
 
 class GetDirectoryNameTests(unittest.TestCase):
 
-	def testRelativePathSlash(self):
+	def test_RelativePathSlash(self):
 		result = GetDirectoryName("foo/")
 		self.assertEqual(result, "foo")
 
-	def testRelativePathNoSlash(self):
+	def test_RelativePathNoSlash(self):
 		result = GetDirectoryName("foo")
 		self.assertEqual(result, "foo")
 
-	def testAbsolutePathSlash(self):
+	def test_AbsolutePathSlash(self):
 		result = GetDirectoryName("/foo/")
 		self.assertEqual(result, "foo")
 
-	def testAbsolutePathNoSlash(self):
+	def test_AbsolutePathNoSlash(self):
 		result = GetDirectoryName("/foo")
 		self.assertEqual(result, "foo")
 
-	def testLongAbsolutePathSlash(self):
+	def test_LongAbsolutePathSlash(self):
 		result = GetDirectoryName("/foo/bar/buz")
 		self.assertEqual(result, "buz")
 
