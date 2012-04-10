428c1df0bf70d2907d581bd7b3491be7aa6e2b75
Mikhail Savkin <savkinmk@gmail.com>
2012-04-11 00:04:23 +0400

    Test run on bare Git repo

--- Tests/BackuperTests.py
+++ Tests/BackuperTests.py
@@ -1,6 +1,6 @@
 __author__ = 'Mikhail K. Savkin'
 
-from os import rmdir
+from os import rmdir, path
 from tempfile import mkdtemp
 from unittest import main, TestCase
 
@@ -8,13 +8,19 @@ from Backuper import Backuper
 
 class MyTestCase(TestCase):
 
-	def test_runNotGitRepo(self):
+	def test_run_NotGitRepo(self):
 		emptyDir = mkdtemp()
 		try:
 			backuper = Backuper(emptyDir)
 			self.assertRaises(ValueError, backuper.run)
 		finally:
 			rmdir(emptyDir)
+
+	def test_run_GitRepo(self):
+		currentDir = path.dirname(path.abspath(__file__))
+		testRepo = path.join(currentDir, "repo.git")
+		backuper = Backuper(testRepo)
+		backuper.run()
 
 if __name__ == '__main__':
 	main()
