d105f4b8e0591e68b06e3071a99eff71af0194f8
Mikhail Savkin <savkinmk@gmail.com>
2012-04-19 15:15:43 +0400

    Test that temp directory is not created, if provided.

--- Tests/BackuperTests.py
+++ Tests/BackuperTests.py
@@ -2,6 +2,7 @@ __author__ = 'Mikhail K. Savkin'
 
 from mock import patch
 from os import rmdir, path
+from shutil import rmtree
 from tempfile import mkdtemp
 from unittest import main, TestCase
 
@@ -38,6 +39,18 @@ class RunTests(TestCase):
 
 		mkdtemp_mock.assert_called_once_with()
 		self.assertFalse(path.exists(tempDir))
+
+	def test_GitRepo_GotTempDir_NoTempDirCreated(self):
+		repoPath = self.__getGitRepoPath()
+		tempDir = mkdtemp()
+		try:
+			with patch('tempfile.mkdtemp') as mkdtemp_mock:
+				backuper = Backuper(repoPath, tempDir)
+				backuper.run()
+
+			self.assertFalse(mkdtemp_mock.called)
+		finally:
+			rmtree(tempDir)
 
 	def __getGitRepoPath(self):
 		currentDir = path.dirname(path.abspath(__file__))
